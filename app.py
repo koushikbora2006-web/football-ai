f3e6e6b8250efde72effb4c53696cbe66354839f
koushik <koushikbora2006@gmail.com>
2026-03-21 21:55:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # -----------------------------
 # Gemini API (FIXED ✅)
 # -----------------------------
-genai.configure(api_key=os.getenv("AIzaSyB16m-xIonmonMQRQLlzJqZ3phRgl1RcHo"))
+genai.configure(api_key=os.getenv("your api key"))
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 CHAT_FILE = "chat_history.json"
